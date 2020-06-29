cac03366cb8753cdf4b3ede61e443d66c38d8a3a
Ronil <ronilsahoo13@gmail.com>
2020-06-29 14:03:41 +0530

    Implementation of pathvariable to filer the records

--- src/main/java/com/paychex/springMVCProject/AlienController.java
+++ src/main/java/com/paychex/springMVCProject/AlienController.java
@@ -3,22 +3,36 @@ package com.paychex.springMVCProject;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.paychex.springMVCProject.model.Alien;
 
 @Controller
-@ResponseBody
+
 public class AlienController {
 
 	@Autowired
 	AlienRepo repo;
-	@GetMapping("/alien")
+	
+	@GetMapping("/aliens")
+	@ResponseBody
 	public List<Alien> getAliens() {
 		List<Alien> alienList = repo.findAll();
 		
 		return alienList;
 	}
+	
+	@GetMapping("alien/{aid}")
+	@ResponseBody
+	public Alien getAlien(@PathVariable("aid") int aid) {
+		
+		Alien a = repo.findById(aid).orElse(new Alien(0, ""));
+		return a;
+	}
+	
+	
 }
